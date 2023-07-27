39719688c50c8fe937571022ff6383f6a6106db1
Alejo2075 <alejsant75@gmail.com>
2023-07-27 13:20:02 -0500

    Change graph data type to int

--- src/main/java/com/alejo2075/webflow/datastrutures/Graph.java
+++ src/main/java/com/alejo2075/webflow/datastrutures/Graph.java
@@ -8,30 +8,30 @@ public class Graph {
 
     private Map<Vertex, List<Vertex>> adjVertices;
     public class Vertex {
-        public String label;
-        Vertex(String label) {
+        public int label;
+        Vertex(int label) {
             this.label = label;
         }
     }
 
-    void addVertex(String label) {
+    void addVertex(int label) {
         adjVertices.putIfAbsent(new Vertex(label), new ArrayList<>());
     }
 
-    void removeVertex(String label) {
+    void removeVertex(int label) {
         Vertex v = new Vertex(label);
         adjVertices.values().stream().forEach(e -> e.remove(v));
         adjVertices.remove(new Vertex(label));
     }
 
-    void addEdge(String label1, String label2) {
+    void addEdge(int label1, int label2) {
         Vertex v1 = new Vertex(label1);
         Vertex v2 = new Vertex(label2);
         adjVertices.get(v1).add(v2);
         adjVertices.get(v2).add(v1);
     }
 
-    void removeEdge(String label1, String label2) {
+    void removeEdge(int label1, int label2) {
         Vertex v1 = new Vertex(label1);
         Vertex v2 = new Vertex(label2);
         List<Vertex> eV1 = adjVertices.get(v1);
