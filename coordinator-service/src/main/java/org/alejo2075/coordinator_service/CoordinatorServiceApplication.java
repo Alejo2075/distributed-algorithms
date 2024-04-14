8fdf57cb7ee1a053448e52eb7effd31c38c47ff0
Alejo2075 <alejsant75@gmail.com>
2024-04-13 19:16:38 -0500

    Initial commit

---
+++ coordinator-service/src/main/java/org/alejo2075/coordinator_service/CoordinatorServiceApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.coordinator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoordinatorServiceApplication.class, args);
+    }
+
+}
---
+++ coordinator-service/src/test/java/org/alejo2075/coordinator_service/CoordinatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.coordinator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
