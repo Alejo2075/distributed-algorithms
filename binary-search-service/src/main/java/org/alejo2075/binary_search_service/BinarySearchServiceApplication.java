1ce6223a3848471e429b801ee161694e762c415d
Alejo2075 <alejsant75@gmail.com>
2024-04-21 00:29:52 -0500

    initial commit for binary search service

---
+++ binary-search-service/src/main/java/org/alejo2075/binary_search_service/BinarySearchServiceApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.binary_search_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarySearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinarySearchServiceApplication.class, args);
+    }
+
+}
---
+++ binary-search-service/src/test/java/org/alejo2075/binary_search_service/BinarySearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.binary_search_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinarySearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
