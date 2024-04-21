c4b49a95745c728aabce0ad8504415fb86a3354d
Alejo2075 <alejsant75@gmail.com>
2024-04-20 23:07:11 -0500

    improved doc config

--- coordinator-service/src/main/java/org/alejo2075/coordinator_service/config/SpringDocConfig.java
+++ coordinator-service/src/main/java/org/alejo2075/coordinator_service/config/SpringDocConfig.java
@@ -1,16 +1,33 @@
 package org.alejo2075.coordinator_service.config;
 
 import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
+
+    @Value("${api.version}")
+    private String apiVersion;
+
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
-                .group("springdoc")
+                .group("springdoc-api-v" + apiVersion)
                 .packagesToScan("org.alejo2075.coordinator_service.controller")
+                .addOpenApiCustomiser(openApi -> {
+                    openApi.info(new io.swagger.v3.oas.models.info.Info()
+                            .title("Coordinator Service API")
+                            .version(apiVersion)
+                            .description("This API handles the coordination of various sorting and searching algorithms in a distributed microservices environment.")
+                            .termsOfService("http://termsOfServiceUrl.com")
+                            .contact(new io.swagger.v3.oas.models.info.Contact()
+                                    .name("Alejandro Santana")
+                                    .url("https://github.com/Alejo2075")
+                                    .email("alejsant75@gmail.com"))
+                            .license(new io.swagger.v3.oas.models.info.License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+                })
                 .build();
     }
 }
