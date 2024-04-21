93a920b709526fba545bdb182a8e93228c1502b6
Alejo2075 <alejsant75@gmail.com>
2024-04-20 23:06:10 -0500

    fixed package

---
+++ coordinator-service/src/main/java/org/alejo2075/coordinator_service/model/dto/request/MergeSortRequest.java
@@ -0,0 +1,17 @@
+package org.alejo2075.coordinator_service.model.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MergeSortRequest {
+
+    @NotNull(message = "The array must not be null.")
+    @Size(min = 1, message = "The array must contain at least one element.")
+    private int[] array;
+}
