0c9e121fa96e79239918b8af9b10d4ffc2ee1fb7
Alejo2075 <alejsant75@gmail.com>
2024-04-20 23:09:19 -0500

    added custom validation

--- coordinator-service/src/main/java/org/alejo2075/coordinator_service/validation/Sorted.java
+++ coordinator-service/src/main/java/org/alejo2075/coordinator_service/validation/Sorted.java
@@ -2,11 +2,8 @@ package org.alejo2075.coordinator_service.validation;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+
+import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = SortedArrayValidator.class)
