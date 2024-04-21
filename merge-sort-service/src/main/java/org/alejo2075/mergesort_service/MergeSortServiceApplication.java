33068721cac0c8119c7dab21eab26b5f27ef5e51
Alejo2075 <alejsant75@gmail.com>
2024-04-21 00:27:32 -0500

    initial commit for merge sort service

---
+++ merge-sort-service/src/main/java/org/alejo2075/mergesort_service/MergeSortServiceApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.mergesort_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeSortServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MergeSortServiceApplication.class, args);
+    }
+
+}
---
+++ merge-sort-service/src/test/java/org/alejo2075/mergesort_service/MergeSortServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alejo2075.mergesort_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeSortServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
