ae44e6d97237c14b70a452bd632580441dd2c79e
Alejo2075 <alejsant75@gmail.com>
2024-04-20 23:07:56 -0500

    added ErrorResponse for Exception message

---
+++ coordinator-service/src/main/java/org/alejo2075/coordinator_service/model/dto/response/ErrorResponse.java
@@ -0,0 +1,14 @@
+package org.alejo2075.coordinator_service.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String status;
+    private String message;
+}
