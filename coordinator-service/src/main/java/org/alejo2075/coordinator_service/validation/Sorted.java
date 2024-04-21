2ae925e4035194eafbba446e73f449623226490d
Alejo2075 <alejsant75@gmail.com>
2024-04-20 22:31:22 -0500

    added validation for binary search sorted array

---
+++ coordinator-service/src/main/java/org/alejo2075/coordinator_service/validation/Sorted.java
@@ -0,0 +1,20 @@
+package org.alejo2075.coordinator_service.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = SortedArrayValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Sorted {
+    String message() default "The array must be sorted";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
+
