aac082aae7db391d52690fc1300c1e0d2eb34b2d
Alejo2075 <alejsant75@gmail.com>
2024-04-28 00:10:43 -0500

    added javadoc

--- service-mergesort-coordinator/src/main/java/org/alejo2075/coordinator_service/controller/CoordinatorController.java
+++ service-mergesort-coordinator/src/main/java/org/alejo2075/coordinator_service/controller/CoordinatorController.java
@@ -11,10 +11,18 @@ import org.alejo2075.coordinator_service.model.dto.request.MergeSortRequest;
 import org.alejo2075.coordinator_service.model.dto.response.MergeSortResponse;
 import org.alejo2075.coordinator_service.service.CoordinatorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+/**
+ * Controller class that handles HTTP requests for sorting algorithms.
+ * This controller is responsible for managing requests to perform sorting operations,
+ * specifically MergeSort in this case, and delegating the processing to the {@link CoordinatorService}.
+ */
 @RestController
 @RequestMapping("/v1/algorithms")
 @Log4j2
@@ -22,11 +30,28 @@ public class CoordinatorController {
 
     private final CoordinatorService coordinatorService;
 
+    /**
+     * Constructs a new CoordinatorController with a dependency on CoordinatorService.
+     *
+     * @param coordinatorService The service that will handle the actual sorting process.
+     */
     public CoordinatorController(CoordinatorService coordinatorService) {
         this.coordinatorService = coordinatorService;
     }
 
-
+    /**
+     * Endpoint to initiate a merge sort on an integer array.
+     * This method takes a JSON request containing an integer array and initiates a merge sort process.
+     * It returns a response indicating the status of the process along with a unique request ID.
+     *
+     * @param request The MergeSortRequest containing the array to be sorted.
+     * @return ResponseEntity containing MergeSortResponse with status and request ID.
+     * @throws JsonProcessingException If an error occurs while processing the request data.
+     *
+     * @apiNote The endpoint validates the incoming request to ensure the array is not null and contains at least one element.
+     * @apiResponse 200 Successful processing - the request was valid, and the sorting process has been initiated.
+     * @apiResponse 400 Invalid request data - the input does not meet the validation criteria.
+     */
     @PostMapping("/mergeSort")
     @Operation(summary = "Performs merge sort on an integer array",
             description = "Submits an integer array for sorting. Returns a response with the processing status.")
