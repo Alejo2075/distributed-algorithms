d54c6590d29590c1507157e9ea4e218ec6043029
Alejo2075 <alejsant75@gmail.com>
2024-04-28 00:12:04 -0500

    implemented method

--- service-mergesort-coordinator/src/main/java/org/alejo2075/coordinator_service/listener/SortEventListener.java
+++ service-mergesort-coordinator/src/main/java/org/alejo2075/coordinator_service/listener/SortEventListener.java
@@ -2,26 +2,96 @@ package org.alejo2075.coordinator_service.listener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
+import org.alejo2075.coordinator_service.model.MergeSortCounter;
+import org.alejo2075.coordinator_service.model.MergeSortResult;
+import org.alejo2075.coordinator_service.model.MergeSortTaskProcessed;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * Listener component for handling sorted segments of arrays processed by workers.
+ * This component listens to Kafka topic for sorted array segments and manages the sorting process
+ * by updating tasks, checking completion, and combining sorted segments.
+ */
 @Component
+@Log4j2
 public class SortEventListener {
 
-    private static final Logger logger = LogManager.getLogger(SortEventListener.class);
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String KAFKA_TOPIC = "mergesort-tasks-processed";
+    private final ObjectMapper objectMapper;
+    private final HashOperations<String, String, Object> hashOps;
 
-    /*@KafkaListener(topics = "mergesort-tasks-processed", groupId = "sort-group")
-    public void onSortEventReceived(String message) {
-        try {
-            SortEvent sortEvent = objectMapper.readValue(message, SortEvent.class);
-            logger.info("Received sorted array: {} elements long.", sortEvent.sortedArr().length);
-        } catch (JsonProcessingException e) {
-            logger.error("Failed to deserialize sort event message", e);
-        } catch (Exception e) {
-            logger.error("Error processing sorted event", e);
+    /**
+     * Constructor for SortEventListener.
+     *
+     * @param redisTemplate The Redis template used for hash operations.
+     * @param objectMapper The Jackson object mapper for JSON processing.
+     */
+    @Autowired
+    public SortEventListener(RedisTemplate<String, Object> redisTemplate, ObjectMapper objectMapper) {
+        this.hashOps = redisTemplate.opsForHash();
+        this.objectMapper = objectMapper;
+    }
+
+    /**
+     * Kafka listener method that triggers when a sorted segment message is received.
+     * This method updates Redis with the task's sorted data, manages counters for sorting completion,
+     * and combines all segments upon completion of all tasks.
+     *
+     * @param message The JSON message received from Kafka.
+     * @throws JsonProcessingException if the message cannot be processed.
+     */
+    @KafkaListener(topics = KAFKA_TOPIC, groupId = "sort-group")
+    public void onSortEventReceived(String message) throws JsonProcessingException {
+        MergeSortTaskProcessed processedTask = objectMapper.readValue(message, MergeSortTaskProcessed.class);
+        log.info("Received sorted segment for request ID: {}, task ID: {}", processedTask.getRequestId(), processedTask.getTaskId());
+
+        // Update the task in Redis with the sorted array
+        hashOps.put("MergeSortTasks", processedTask.getTaskId(), processedTask);
+
+        // Update the counter and check if all segments are sorted
+        MergeSortCounter counter = (MergeSortCounter) hashOps.get("MergeSortCounters", processedTask.getRequestId());
+        assert counter != null;
+        counter.setCounter(counter.getCounter() - 1);
+        hashOps.put("MergeSortCounters", processedTask.getRequestId(), counter);
+        log.info("Updated counter for request ID: {}, remaining tasks: {}", processedTask.getRequestId(), counter.getCounter());
+
+        // Check if all segments are sorted
+        if (counter.getCounter() == 0) {
+            log.info("All segments sorted for request ID: {}", processedTask.getRequestId());
+            int[] sortedArray = combineAndSortAllSegments(counter, processedTask.getRequestId());
+
+            // Store the sorted array in Redis
+            MergeSortResult result = new MergeSortResult(processedTask.getRequestId(), sortedArray);
+            hashOps.put("MergeSortResults", processedTask.getRequestId(), result);
+            log.info("Sorted array stored in Redis for request ID: {}", processedTask.getRequestId());
+
+            // Clean up tasks and counter from Redis
+            Arrays.stream(counter.getTaskIds()).forEach(taskId -> hashOps.delete("MergeSortTasks", taskId));
+            hashOps.delete("MergeSortCounters", processedTask.getRequestId());
+            log.info("Cleaned up all tasks and counter for request ID: {}", processedTask.getRequestId());
         }
-    }*/
+    }
+
+    /**
+     * Combines and sorts all segments stored in Redis once all tasks are completed.
+     *
+     * @param counter The counter containing IDs of all tasks.
+     * @param requestId The request ID associated with these tasks.
+     * @return A sorted array combining all segments.
+     */
+    protected int[] combineAndSortAllSegments(MergeSortCounter counter, String requestId) {
+        return Arrays.stream(counter.getTaskIds())
+                .map(taskId -> (MergeSortTaskProcessed) hashOps.get("MergeSortTasks", taskId)).filter(Objects::nonNull)
+                .flatMapToInt(task -> Arrays.stream(task.getArraySortedSegment()))
+                .sorted()
+                .toArray();
+    }
 }
--- service-mergesort-coordinator/src/main/java/org/alejo2075/coordinator_service/service/CoordinatorServiceImpl.java
+++ service-mergesort-coordinator/src/main/java/org/alejo2075/coordinator_service/service/CoordinatorServiceImpl.java
@@ -3,24 +3,100 @@ package org.alejo2075.coordinator_service.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
+import org.alejo2075.coordinator_service.model.MergeSortCounter;
+import org.alejo2075.coordinator_service.model.MergeSortTask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+/**
+ * Service class to handle the coordination of MergeSort tasks.
+ * This service is responsible for initiating and managing the lifecycle of merge sort operations,
+ * including distributing tasks to Kafka and storing task and state information in Redis.
+ */
 @Service
 @Log4j2
-public class CoordinatorServiceImpl implements CoordinatorService{
+public class CoordinatorServiceImpl implements CoordinatorService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
     private static final String KAFKA_TOPIC = "mergesort-tasks-to-process";
+    private static final int PARTITION_NUMBER = 2;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public CoordinatorServiceImpl(KafkaTemplate<String, String> kafkaTemplate) {
+    private final HashOperations<String, String, Object> hashOps;
+
+    /**
+     * Constructs a new CoordinatorServiceImpl with required components.
+     *
+     * @param kafkaTemplate the Kafka template for publishing messages.
+     * @param redisTemplate the Redis template for managing Redis operations.
+     */
+    @Autowired
+    public CoordinatorServiceImpl(KafkaTemplate<String, String> kafkaTemplate, RedisTemplate<String, Object> redisTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+        this.hashOps = redisTemplate.opsForHash();
     }
 
+    /**
+     * Starts the merge sort process for a given request ID and array.
+     * This method splits the array into segments, publishes each segment to Kafka,
+     * and stores task and counter information in Redis.
+     *
+     * @param requestId the unique ID for this sorting request.
+     * @param array the array to sort.
+     * @throws JsonProcessingException if an error occurs during object mapping.
+     */
     @Override
     public void startMergeSortProcess(String requestId, int[] array) throws JsonProcessingException {
+        log.info("Starting merge sort process for request ID: {}", requestId);
 
+        List<int[]> segments = splitArray(array, PARTITION_NUMBER);
+        MergeSortCounter counter = new MergeSortCounter(0, requestId, new String[segments.size()]);
+        log.debug("Array has been split into {} parts for request ID: {}", segments.size(), requestId);
+
+        for (int[] segment : segments) {
+            String taskId = UUID.randomUUID().toString();
+            MergeSortTask task = new MergeSortTask(requestId, taskId, segment);
+            String message = objectMapper.writeValueAsString(task);
+            kafkaTemplate.send(KAFKA_TOPIC, message);
+            log.info("Published task {} for request {} to Kafka topic {}", taskId, requestId, KAFKA_TOPIC);
+
+            counter.getTaskIds()[counter.getCounter()] = taskId;
+            counter.setCounter(counter.getCounter() + 1);
+            hashOps.put("MergeSortTasks", taskId, task);
+            log.info("Saved task {} for request {} to Redis", taskId, requestId);
+        }
+
+        hashOps.put("MergeSortCounters", requestId, counter);
+        log.info("Counter for request ID {} has been updated and saved to Redis", requestId);
+    }
+
+    /**
+     * Splits an array into a specified number of parts.
+     *
+     * @param array the array to split.
+     * @param parts the number of parts to split the array into.
+     * @return a list of array segments.
+     */
+    protected List<int[]> splitArray(int[] array, int parts) {
+        int length = array.length;
+        int n = length / parts;
+        int r = length % parts;
+        List<int[]> segments = new ArrayList<>();
+
+        for (int i = 0; i < parts; i++) {
+            int start = i * n + Math.min(i, r);
+            int end = start + n + (i < r ? 1 : 0);
+            int[] segment = new int[end - start];
+            System.arraycopy(array, start, segment, 0, segment.length);
+            segments.add(segment);
+        }
+        return segments;
     }
 }
